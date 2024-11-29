0fe2828f88f9e9dced30fe5747fb0bc7a3764071
Pranav Bagal <jugad360@gmail.com>
2024-11-29 11:21:56 +0000

    Remove .env from git ignore

--- Backend_D-voting/db/connect.js
+++ Backend_D-voting/db/connect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connection_DB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(
+      process.env.DATABASE_URL
+
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
